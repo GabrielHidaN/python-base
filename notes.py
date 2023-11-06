18b20f200cdf834eeb65878335ad607842cc751a
Gabriel Victor <victorbielgv7@gmail.com>
2023-11-06 19:53:26 -0300

    "06/11/23"

--- notes.py
+++ notes.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env pythob3
+#!/usr/bin/env python3
 """Bloco de notas
 
 $ notes.py new "Minha Nota"
 tag: tech
-text:
+text: 
 Anotacao geral sobre carreira de tecnologia
 
-$ notes.py read tag
+$ notes.py 1 tech
 ...
 ...
 """
-
-__version__="0.1.0"
+__version__ = "0.1.0"
 
 import os
 import sys
@@ -27,10 +26,10 @@ if not arguments:
     sys.exit(1)
 
 if arguments[0] not in cmds:
-    print(f"Invalid Command {arguments[0]}")
+    print(f"Invalid command {arguments[0]}")
 
 if arguments[0] == "read":
-    #leitura das notas
+    # leitura das notas
     for line in open(filepath):
         title, tag, text = line.split("\t")
         if tag.lower() == arguments[1].lower():
@@ -40,14 +39,12 @@ if arguments[0] == "read":
             print()
 
 if arguments[0] == "new":
-    # criação das notas
-    title = arguments[1] # TODO tratar exception
+    title = arguments[1]
     text = [
         f"{title}",
         input("tag:").strip(),
         input("text:\n").strip(),
-        
     ]
-    #\t - tsv
+    # \t - tsv
     with open(filepath, "a") as file_:
         file_.write("\t".join(text) + "\n")
